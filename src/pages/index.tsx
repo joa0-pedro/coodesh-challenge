082a97c1aa17e59a3314a39dad2d4df3df582fd9
joa0-pedro <joaopedroamorim56@gmail.com>
2023-06-10 12:17:12 -0300

    feat: adding Index page, custon themes and show email component

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { Box, Center, Text } from "@chakra-ui/react";
+import { Email } from "../components/Email";
+
+export function Index() {
+  return (
+    <Box bgColor={"white"}>
+      <Center>
+        <Text
+          fontSize={"5xl"}
+          fontWeight={"bold"}
+          fontFamily={"sans-serif"}
+          color={"gray.900"}
+        >
+          DropMail.me
+        </Text>
+      </Center>
+      <Box border={"2px"} borderRadius={8} h={"54rem"} mx={"0.8rem"}>
+        <Email />
+      </Box>
+    </Box>
+  );
+}
