4e9d6119b6bd7a32aba988cebf7132c3e5ab3017
joa0-pedro <joaopedroamorim56@gmail.com>
2023-06-12 15:59:41 -0300

    Connecting to DropMail Proxy

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -3,24 +3,9 @@ export * from "./queries";
 
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-// const endpoint =
-//   "wss://dropmail.me/api/graphql/web-test-20230608KBE1x/websocket";
-
 export const client = new ApolloClient({
-  // link: new WebSocketLink(
-  //   new SubscriptionClient(endpoint, { reconnect: true })
-  // ),
-
   link: new HttpLink({
-    uri: "https://dropmail.me/api/graphql/web-test-20230608KBE1x",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true",
-    },
-    fetch,
+    uri: "https://dropmailproxy.onrender.com/graphql/web-test-20230608KBE1x",
   }),
-
-  // uri: "https://dropmail.me/api/graphql/web-test-20230608KBE1x",
   cache: new InMemoryCache(),
 });
